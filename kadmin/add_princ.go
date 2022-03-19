66d76b4c487f2012f2a597d868321d42e408e737
Mellywins <zouaghi.wow1919@gmail.com>
2022-03-19 18:45:23 +0100

    AddPrincipal + AddPrincipalAttributes builders and interfaces

--- kadmin/add_princ.go
+++ kadmin/add_princ.go
@@ -1,6 +1,14 @@
 package ktadmin
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
+
+type Executable interface {
+	ParseCommand()
+	Exec()
+}
 
 type AddPrincipalType struct {
 	randkey     int    // Sets the key of the principal to a random value.
@@ -43,9 +51,10 @@ type AddPrincipalAttributes struct {
 var IntToSymbolMap = map[int]string{0: "-", 1: "+"}
 
 /*
-Instantiate a new Add_Principal command runner
+Instantiate a new Add_Principal command runner.
+Not setting a flag to a value of your chooseing implies that the KDC command will use its default value.
 */
-func AddPrincipal() *AddPrincipalType {
+func AddPrincipal(atts AddPrincipalAttributes) *AddPrincipalType {
 	return &AddPrincipalType{
 		randkey:      -1,
 		nokey:        -1,
@@ -58,26 +67,20 @@ func AddPrincipal() *AddPrincipalType {
 		password:     "",
 		maxrenewlife: "",
 		principal:    "",
-		attributes: AddPrincipalAttributes{
-			allow_postdated:           -1,
-			allow_forwardable:         -1,
-			allow_renewable:           -1,
-			allow_proxiable:           -1,
-			allow_dup_key:             -1,
-			requires_preauth:          -1,
-			requires_hwauth:           -1,
-			ok_as_delegate:            -1,
-			allow_svr:                 -1,
-			allow_tgs_req:             -1,
-			allow_tix:                 -1,
-			needchange:                -1,
-			password_changing_service: -1,
-			ok_to_auth_as_delegate:    -1,
-			no_auth_data_required:     -1,
-		},
+		attributes:   atts,
 	}
 }
 
+func (apt *AddPrincipalType) ParseCommand() {
+	fmt.Println("Parsed Command")
+}
+func (apt *AddPrincipalType) Exec() {
+	fmt.Println("Execution ...")
+}
+func (apt *AddPrincipalType) WithAttributes(atts AddPrincipalAttributes) *AddPrincipalType {
+	apt.attributes = atts
+	return apt
+}
 func (apt *AddPrincipalType) WithExpDate(date string) *AddPrincipalType {
 	apt.expdate = date
 	return apt
@@ -128,4 +131,113 @@ func (apt *AddPrincipalType) WithPrincipal(name string) *AddPrincipalType {
 func (apt *AddPrincipalType) sanitizeAttributes() *AddPrincipalType {
 
 	return apt
+}
+
+// -allow_postdated(0) prohibits this principal from obtaining postdated tickets. +allow_postdated(1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetPostdated(n int) *AddPrincipalAttributes {
+	apatts.allow_postdated = n
+	return apatts
+}
+
+// -allow_forwardable(0) prohibits this principal from obtaining forwardable tickets. +allow_forwardable (1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetForwardable(n int) *AddPrincipalAttributes {
+	apatts.allow_forwardable = n
+	return apatts
+}
+
+// -allow_renewable(0) prohibits this principal from obtaining renewable tickets. +allow_renewable(1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetRenewable(n int) *AddPrincipalAttributes {
+	apatts.allow_renewable = n
+	return apatts
+}
+
+// -allow_proxiable(0) prohibits this principal from obtaining proxiable tickets. +allow_proxiable(1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetProxiable(n int) *AddPrincipalAttributes {
+	apatts.allow_proxiable = n
+	return apatts
+}
+
+// -allow_dup_skey(0) disables user-to-user authentication for this principal by prohibiting this principal from obtaining a session key for another user. +allow_dup_skey (1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetDupKey(n int) *AddPrincipalAttributes {
+	apatts.allow_dup_key = n
+	return apatts
+}
+
+// +requires_preauth(1) requires this principal to preauthenticate before being allowed to kinit. -requires_preauth(0) clears this flag. When +requires_preauth is set on a service principal, the KDC will only issue service tickets for that service principal if the client’s initial authentication was performed using preauthentication.
+func (apatts *AddPrincipalAttributes) SetPreAuth(n int) *AddPrincipalAttributes {
+	apatts.requires_preauth = n
+	return apatts
+}
+
+// +requires_hwauth(1) requires this principal to preauthenticate using a hardware device before being allowed to kinit. -requires_hwauth(0) clears this flag. When +requires_hwauth is set on a service principal, the KDC will only issue service tickets for that service principal if the client’s initial authentication was performed using a hardware device to preauthenticate.
+func (apatts *AddPrincipalAttributes) SetHwAuth(n int) *AddPrincipalAttributes {
+	apatts.requires_hwauth = n
+	return apatts
+}
+
+// +ok_as_delegate(1) sets the okay as delegate flag on tickets issued with this principal as the service. Clients may use this flag as a hint that credentials should be delegated when authenticating to the service. -ok_as_delegate(0) clears this flag.
+func (apatts *AddPrincipalAttributes) SetOkAsDelegate(n int) *AddPrincipalAttributes {
+	apatts.ok_as_delegate = n
+	return apatts
+}
+
+// -allow_svr(0) prohibits the issuance of service tickets for this principal. +allow_svr clears this flag(1).
+func (apatts *AddPrincipalAttributes) SetSvr(n int) *AddPrincipalAttributes {
+	apatts.allow_svr = n
+	return apatts
+}
+
+// -allow_tgs_req(0) specifies that a Ticket-Granting Service (TGS) request for a service ticket for this principal is not permitted. +allow_tgs_req(1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetTgsReq(n int) *AddPrincipalAttributes {
+	apatts.allow_tgs_req = n
+	return apatts
+}
+
+// -allow_tix(0) forbids the issuance of any tickets for this principal. +allow_tix (1) clears this flag.
+func (apatts *AddPrincipalAttributes) SetTix(n int) *AddPrincipalAttributes {
+	apatts.allow_tix = n
+	return apatts
+}
+
+// +needchange(1) forces a password change on the next initial authentication to this principal. -needchange(0) clears this flag.
+func (apatts *AddPrincipalAttributes) SetNeedChange(n int) *AddPrincipalAttributes {
+	apatts.needchange = n
+	return apatts
+}
+
+// +password_changing_service(1) marks this principal as a password change service principal.
+func (apatts *AddPrincipalAttributes) SetPasswordChangingService() *AddPrincipalAttributes {
+	apatts.password_changing_service = 1
+	return apatts
+}
+
+// +ok_to_auth_as_delegate allows this principal to acquire forwardable tickets to itself from arbitrary users, for use with constrained delegation.
+func (apatts *AddPrincipalAttributes) SetOkToAuthAsDelegate() *AddPrincipalAttributes {
+	apatts.ok_to_auth_as_delegate = 1
+	return apatts
+}
+
+// +no_auth_data_required prevents PAC or AD-SIGNEDPATH data from being added to service tickets for the principal.
+func (apatts *AddPrincipalAttributes) SetNoAuthDataRequired() *AddPrincipalAttributes {
+	apatts.no_auth_data_required = 1
+	return apatts
+}
+func CreateAddPrincipalAttributes() *AddPrincipalAttributes {
+	return &AddPrincipalAttributes{
+		allow_postdated:           -1,
+		allow_forwardable:         -1,
+		allow_renewable:           -1,
+		allow_proxiable:           -1,
+		allow_dup_key:             -1,
+		requires_preauth:          -1,
+		requires_hwauth:           -1,
+		ok_as_delegate:            -1,
+		allow_svr:                 -1,
+		allow_tgs_req:             -1,
+		allow_tix:                 -1,
+		needchange:                -1,
+		password_changing_service: -1,
+		ok_to_auth_as_delegate:    -1,
+		no_auth_data_required:     -1,
+	}
 }
--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 
 	internal "github.com/Mellywins/gokrb5-kdc-wrapper/internal"
+	kadmin "github.com/Mellywins/gokrb5-kdc-wrapper/kadmin"
 )
 
 func main() {
 	status, err := internal.EnsureServiceIsRunning()
-
 	fmt.Printf("Is service running: %v, status is : %s", err, status)
+
+	atts := kadmin.CreateAddPrincipalAttributes().SetDupKey(0)
+	addP := kadmin.AddPrincipal(*atts).
+		WithPrincipal("oussema").
+		WithExpDate("12/06/1999").
+		WithKvno(0).
+		WithPassword("oussema1999").
+		WithClearPolicy().
+		ParseCommand().
+		Exec()
+
 }
