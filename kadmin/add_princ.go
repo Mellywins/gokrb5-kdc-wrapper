a6c4f81844fdd85ce0e36a594f1670b7f6a45d2a
Mellywins <zouaghi.wow1919@gmail.com>
2022-03-19 17:12:58 +0100

    more Builder blocks

--- kadmin/add_princ.go
+++ kadmin/add_princ.go
@@ -1,5 +1,7 @@
 package ktadmin
 
+import "log"
+
 type AddPrincipalType struct {
 	randkey     int    // Sets the key of the principal to a random value.
 	nokey       int    // Note that you cant use randkey and no key. Using them both will result in an error during the command parsing
@@ -16,6 +18,9 @@ type AddPrincipalType struct {
 	attributes   string // Attribute list, visit https://web.mit.edu/kerberos/krb5-1.12/doc/admin/admin_commands/kadmin_local.html for more information.
 }
 
+/*
+Instantiate a new Add_Principal command runner
+*/
 func AddPrincipal() *AddPrincipalType {
 	return &AddPrincipalType{
 		randkey:      0,
@@ -47,5 +52,43 @@ func (apt *AddPrincipalType) WithKvno(kvno int) *AddPrincipalType {
 }
 func (apt *AddPrincipalType) WithPolicy(policy string) *AddPrincipalType {
 	apt.policy = policy
+	return apt
+}
+func (apt *AddPrincipalType) WithRandKey() *AddPrincipalType {
+	if apt.nokey == 1 {
+		log.Fatal("Cannot Use RANDKEY while NOKEY flag is previously specified")
+	}
+	apt.randkey = 1
+	return apt
+}
+func (apt *AddPrincipalType) WithNoKey() *AddPrincipalType {
+	if apt.randkey == 1 {
+		log.Fatal("Cannot use NoKEY flag while RANDKEY flag is previously specified")
+	}
+	apt.nokey = 1
+	return apt
+}
+func (apt *AddPrincipalType) WithMaxLife(max_life_date string) *AddPrincipalType {
+	apt.maxlife = max_life_date
+	return apt
+}
+func (apt *AddPrincipalType) WithClearPolicy() *AddPrincipalType {
+	apt.clearpolicy = 1
+	return apt
+}
+func (apt *AddPrincipalType) WithPassword(pw string) *AddPrincipalType {
+	apt.password = pw
+	return apt
+}
+func (apt *AddPrincipalType) WithPrincipal(name string) *AddPrincipalType {
+	apt.principal = name
+	return apt
+}
+func (apt *AddPrincipalType) AllowPostdated() *AddPrincipalType {
+
+}
+
+func (apt *AddPrincipalType) sanitizeAttributes() *AddPrincipalType {
+
 	return apt
 }
